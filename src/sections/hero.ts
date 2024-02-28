66952ff80bd3b11113a3a619cc8e6c031c2878cf
rohit <rohittp0@github.com>
2024-02-28 18:56:19 +0530

    hero done ( for now )

--- src/sections/hero.ts
+++ src/sections/hero.ts
@@ -1,1 +1,9 @@
 import "../scss/hero.scss";
+
+const bootText = document.getElementById("bootText");
+
+const getBootFormat = (text: string) => `<p><span class="text-green-400">[&nbsp;&nbsp;OK&nbsp;&nbsp;]</span>${text}</p>`
+
+for (let i = 0; i < 100; i++) {
+  bootText.innerHTML += getBootFormat("Something cool about WIT Conference, ideally this long.")
+}
