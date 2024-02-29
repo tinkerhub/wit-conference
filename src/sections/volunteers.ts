1699fe458bf5a6cd672cbf254226df5d621fa4a3
Alexmathai2001 <alexmathai07@gmail.com>
2024-02-29 09:57:09 +0530

    made the whole theme to black and font to mono

--- src/sections/volunteers.ts
+++ src/sections/volunteers.ts
@@ -25,7 +25,7 @@ seelessButton.addEventListener('click', function() {
 
 let volunteerCardsToShow = 4; // Number of initial volunteer cards to show
 const volunteersData = [
-  { name: "Alex first", role: "Volunteer", imageSrc: "/img/volunteers/alex.jpg" },
+  { name: "Alex Mathai", role: "Volunteer", imageSrc: "/img/volunteers/alex.jpg" },
   { name: "Alex Mathai", role: "Volunteer", imageSrc: "/img/volunteers/volunteer-fake-img.jpg" },
   { name: "Alex Mathai", role: "Volunteer", imageSrc: "/img/volunteers/volunteer-fake-img.jpg" },
   { name: "Alex Mathai", role: "Volunteer", imageSrc: "/img/volunteers/volunteer-fake-img.jpg" },
@@ -52,12 +52,12 @@ function showVolunteerCards() {
         const volunteer = volunteersData[i];
         container.innerHTML += `
             <!-- Repeat the card structure for each volunteer -->
-            <div class="bg-white hover:scale-105 rounded-lg px-2 w-full h-44 md:h-40 flex justify-end transition-transform duration-300 ease-in-out transform">
+            <div class="bg-black hover:scale-105 rounded-lg px-2 w-full h-44 md:h-40 flex justify-end transition-transform duration-300 ease-in-out transform">
                 <div class="bg-red-300 w-10/12 h-3/5 md:h-4/6 lg:h-2/3 mt-10 rounded-2xl flex gap-6">
                     <img src="${volunteer.imageSrc}" class="h-full rounded-full aspect-square -ms-14 md:-ms-10 shadow-xl object-cover" alt="">
                     <div class="py-5">
-                        <p class="text-xl md:text-md font-bold">${volunteer.name}</p>
-                        <p class="font-semibold">${volunteer.role}</p>
+                        <p class="text-xl md:text-md font-bold font-mono">${volunteer.name}</p>
+                        <p class="font-semibold font-mono ">${volunteer.role}</p>
                     </div>
                 </div>
             </div>
